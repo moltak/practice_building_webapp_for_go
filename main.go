ae6e2e8cb47914f9e777f9fd58f6747f235d65a5
moltak <moltak@gmail.com>
2015-09-12 16:25:47 -0700

    The negroni middleware has been added.

--- main.go
+++ main.go
@@ -1,63 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"os"
+	"log"
 
-	"BuildingWebApp/Godeps/_workspace/src/github.com/julienschmidt/httprouter"
+	"github.com/codegangsta/negroni"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
+	n := negroni.New(
+		negroni.NewRecovery(),
+		negroni.HandlerFunc(MyMiddleware),
+		negroni.NewLogger(),
+		negroni.NewStatic(http.Dir("public")),
+	)
+	n.Run(":3000")
+}
+
+func MyMiddleware(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	log.Println("Loggin on the way there...")
+
+	if r.URL.Query().Get("password") == "secret123" {
+		next(rw, r)
+	} else {
+		http.Error(rw, "Not Authorized", 401)
 	}
 
-	// handler
-	r := httprouter.New()
-	r.GET("/", HomeHandler)
-
-	// Posts collection
-	r.GET("/posts", PostIndexHandler)
-	r.POST("/posts", PostsCreateHandler)
-
-	// Posts singular
-	r.GET("/posts/:id", PostShowHandler)
-	r.PUT("/posts/:id", PostUpdateHandler)
-	r.GET("/posts/:id/edit", PostEditHandler)
-
-	fmt.Println("Starting server on : " + port)
-
-	http.ListenAndServe(":" + port, r)
-}
-
-
-func HomeHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintf(rw, "Home")
-}
-
-func PostIndexHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintf(rw, "posts index")
-}
-
-func PostsCreateHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprint(rw, "posts create")
-}
-
-func PostShowHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	id := p.ByName("id")
-	fmt.Fprintln(rw, "showing post", id)
-}
-
-func PostUpdateHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintln(rw, "post update")
-}
-
-func PostDeleteHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintln(rw, "post delete")
-}
-
-func PostEditHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintln(rw, "post edit")
+	log.Println("Loggin on the way back...")
 }
