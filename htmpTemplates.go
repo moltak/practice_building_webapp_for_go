dafd473d46ae489502e69081649716d8c0a36dfa
moltak <moltak@gmail.com>
2015-09-12 21:07:43 -0700

    Reduce overhaed from template.ParseFiles

--- htmpTemplates.go
+++ htmpTemplates.go
@@ -11,20 +11,22 @@ type Book struct {
 	Author string
 }
 
+var tmpl *template.Template
+
 func main() {
+	parseHtml()
 	http.HandleFunc("/", ShowBooks)
 	http.ListenAndServe(":3000", nil)
 }
 
+func parseHtml() {
+	fp := path.Join("templates", "index.html")
+	temp, _ := template.ParseFiles(fp)
+	tmpl = temp
+}
+
 func ShowBooks(w http.ResponseWriter, r *http.Request) {
 	book := Book{"LA Weekly", "LAWEEKLY.COM"}
-
-	fp := path.Join("templates", "index.html")
-	tmpl, err := template.ParseFiles(fp)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
 
 	if err := tmpl.Execute(w, book); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
